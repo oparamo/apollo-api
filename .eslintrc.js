8828dd2202e4da917beca344bdd9573c5802b183
Oscar Paramo <oscarparamo@me.com>
2015-12-20 14:04:33 -0600

    refactor(eslint): Switch to hapi.js eslint configuration.

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  'extends': 'hapi',
+  'root': true,
+  'rules': {
+    'hapi/hapi-scope-start': 0,
+    'indent': [2, 2, { 'SwitchCase': 1 }]
+  }
+};
--- config/index.js
+++ config/index.js
@@ -1,11 +1,10 @@
 'use strict';
 
 // set up for default/development environment
-let config = {
+const config = {
   CORS: [process.env.APOLLO_CORS || '*'],
   HOST: process.env.APOLLO_HOST || '0.0.0.0',
   PORT: process.env.APOLLO_PORT || 4000
 };
-
 
 module.exports = config;
