0318ab4df57a6dd77aab3e788918c53bc748d7eb
Oscar Paramo <oscarparamo@me.com>
2015-12-19 21:11:21 -0600

    type(chore): Update eslint configuration and add basic Hapi server structure.

---
+++ config/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// set up for default/development environment
+let config = {
+  CORS: [process.env.APOLLO_CORS || '*'],
+  HOST: process.env.APOLLO_HOST || '0.0.0.0',
+  PORT: process.env.APOLLO_PORT || 4000
+};
+
+
+module.exports = config;
