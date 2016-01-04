57c7591a5a0320de6982d1d050822e85a3e2d717
Oscar Paramo <oscar@vin.li>
2016-01-03 19:15:31 -0600

    refactor(lab): Refactor project structure for testing.

--- app/index.js
+++ app/index.js
@@ -1,48 +1,14 @@
 'use strict';
 
-const Hapi = require('hapi');
 const Util = require('util');
 
 const Config = require('../config');
+const Server = require('./server');
 
-const server = new Hapi.Server({
-  connections: {
-    routes: {
-      // allowed origin servers and allow user credentials
-      cors: {
-        origin: Config.CORS,
-        credentials: true
-      },
-      // json formatting
-      json: { space: 2 }
-    }
-  }
-});
-
-// server host and port configuration
-server.connection({
-  host: Config.HOST,
-  port: Config.PORT
-});
-
-// start server when plugins registered succesfully
-server.register({ register: require('./plugins') }, (err) => {
+Server.start((err) => {
   if (err) {
-    throw err;
+    return Util.log('Error:', err.message);
   };
 
-  server.start((err) => {
-    if (err) {
-      return Util.log('Error:', err.message);
-    };
-
-    Util.log(`Server started on ${Config.HOST}:${Config.PORT}`);
-  });
+  Util.log(`Server started on ${Config.HOST}:${Config.PORT}`);
 });
-
-// gracefully stop server upon receipt of process termination signal
-process.on('SIGTERM', () => {
-  server.stop({ timeout: 5 * 1000 }, () => process.exit(0));
-});
-
-module.exports = server;
---
+++ app/server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const Config = require('../config');
+
+const server = new Hapi.Server({
+  connections: {
+    routes: {
+      // allowed origin servers and allow user credentials
+      cors: {
+        origin: Config.CORS,
+        credentials: true
+      },
+      // json formatting
+      json: { space: 2 }
+    }
+  }
+});
+
+// server host and port configuration
+server.connection({
+  host: Config.HOST,
+  port: Config.PORT
+});
+
+// register plugins
+server.register({ register: require('./plugins') }, (err) => {
+  /* $lab:coverage:off$ */
+  if (err) {
+    throw err;
+  };
+  /* $lab:coverage:on$ */
+});
+
+module.exports = server;
